7c403680f741f8871bc49915379f887798043f36
Tanaka Takaaki <kongmings.trap@gmail.com>
2019-09-25 14:53:34 +0900

    Add GestureStateSampler.swift

---
+++ GestureStateSampler.swift
@@ -0,0 +1,15 @@
+struct CounterView: View {
+    @GestureState var isDetectingLongPress = false
+
+    var body: some View {
+        let press = LongPressGesture(minimumDuration: 1)
+            .updating($isDetectingLongPress) { currentState, gestureState, transaction in
+                gestureState = currentState
+            }
+
+        return Circle()
+            .fill(isDetectingLongPress ? Color.yellow : Color.green)
+            .frame(width: 100, height: 100, alignment: .center)
+            .gesture(press)
+    }
+}
